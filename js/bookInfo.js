208aa407727e79d39b27bf28a3317b18592bd1b7
linyh <linyh@sunsharing.com.cn>
2016-09-26 16:26:11 +0800

    内页详细

--- js/bookInfo.js
+++ js/bookInfo.js
@@ -16,8 +16,7 @@ $(function() {
             return $('#tpl_first').html();
         },
         bind: function () {
-            $('#container').on('click', '#showActionSheet', function () {
-                alert(132);
+            $('#showActionSheet').click(function () {
                 var mask = $('#mask');
                 var weuiActionsheet = $('#weui_actionsheet');
                 weuiActionsheet.addClass('weui_actionsheet_toggle');
@@ -41,6 +40,31 @@ $(function() {
                     })
                 }
             });
+            // $('#container').on('click', '#showActionSheet', function () {
+            //     alert(132);
+            //     var mask = $('#mask');
+            //     var weuiActionsheet = $('#weui_actionsheet');
+            //     weuiActionsheet.addClass('weui_actionsheet_toggle');
+            //     mask.show()
+            //         .focus()//加focus是为了触发一次页面的重排(reflow or layout thrashing),使mask的transition动画得以正常触发
+            //         .addClass('weui_fade_toggle').one('click', function () {
+            //         hideActionSheet(weuiActionsheet, mask);
+            //     });
+            //     $('#actionsheet_cancel').one('click', function () {
+            //         hideActionSheet(weuiActionsheet, mask);
+            //     });
+            //     mask.unbind('transitionend').unbind('webkitTransitionEnd');
+            //
+            //     function hideActionSheet(weuiActionsheet, mask) {
+            //         weuiActionsheet.removeClass('weui_actionsheet_toggle');
+            //         mask.removeClass('weui_fade_toggle');
+            //         mask.on('transitionend', function () {
+            //             mask.hide();
+            //         }).on('webkitTransitionEnd', function () {
+            //             mask.hide();
+            //         })
+            //     }
+            // });
         }
     };
 
