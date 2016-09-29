aae42f8df8f3f7798043dc1eaa47951f17684134
lgbcscs <2507243653@qq.com>
2016-09-30 00:40:58 +0800

    检查项

--- js/addCheckInfo.js
+++ js/addCheckInfo.js
@@ -5,12 +5,29 @@ $(function() {
         leaveTimeout: 0
     });
 
+    //获取url中的参数
+    function getUrlParam(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+        var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+        if (r != null) return decodeURI(r[2]); return null; //返回参数值
+    }
+
     // list
     var list = {
         url: '/list',
         className: 'list',
         render: function () {
             return $('#tpl_list').html();
+        },
+        bind :function(){
+            var indexofMyVal = window.location.href.indexOf('#/');
+            var _myUrl = window.location.href.substr(0,indexofMyVal+2);
+            if(getUrlParam('mytype')=="list"){
+                //window.location.href=_myUrl+'list';
+            }else{
+                //$(".info-title .btn_add").trigger("click");
+                router.setDefault('/info');
+            }
         }
     };
 
--- js/bookInfo.js
+++ js/bookInfo.js
@@ -8,67 +8,91 @@ $(function() {
         leaveTimeout: 0
     });
 
-    // first
-    var first = {
-        url: '/first',
-        className: 'first',
+    function setUrl (type,domObj,currentClass,infoSize){
+        var _length = currentClass.length;
+        if(type == "back"){
+            var _number = parseInt(currentClass.substring(5,_length))-1;
+            if(currentClass =="item_1"){
+                domObj.attr({"href":"#/item_1"});
+            }else{
+                domObj.attr({"href":"#/item_"+_number});
+            }
+        }else{
+            var _number = parseInt(currentClass.substring(5,_length))+1;
+            if(_number==(infoSize+1)){
+                domObj.attr({"href":"#/item_"+infoSize});
+            }else{
+                domObj.attr({"href":"#/item_"+_number});
+            }
+        }
+        //if(currentClass =="item_1"){
+        //    domObj.attr({"href":"#/item_1"});
+        //}else if(currentClass =="item_100"){
+        //    domObj.attr({"href":"#/item_100"});
+        //}else{
+        //    domObj.attr({"href":"#/item_"+_number});
+        //}
+    }
+
+    // default
+    var item_1 = {
+        url: '/item_1',
+        className: 'item_1',
         render: function () {
-            return $('#tpl_first').html();
+            return $('#tpl_1').html();
         },
         bind: function () {
-            $('#showActionSheet').click(function () {
-                var mask = $('#mask');
-                var weuiActionsheet = $('#weui_actionsheet');
-                weuiActionsheet.addClass('weui_actionsheet_toggle');
-                mask.show()
-                    .focus()//加focus是为了触发一次页面的重排(reflow or layout thrashing),使mask的transition动画得以正常触发
-                    .addClass('weui_fade_toggle').one('click', function () {
-                    hideActionSheet(weuiActionsheet, mask);
-                });
-                $('#actionsheet_cancel').one('click', function () {
-                    hideActionSheet(weuiActionsheet, mask);
-                });
-                mask.unbind('transitionend').unbind('webkitTransitionEnd');
-
-                function hideActionSheet(weuiActionsheet, mask) {
-                    weuiActionsheet.removeClass('weui_actionsheet_toggle');
-                    mask.removeClass('weui_fade_toggle');
-                    mask.on('transitionend', function () {
-                        mask.hide();
-                    }).on('webkitTransitionEnd', function () {
-                        mask.hide();
-                    })
-                }
+            $("#go_back").click(function(){
+                var currentItem = $("#container >div").attr("class");
+                var _this = $(this);
+                setUrl("back",_this,currentItem,2);
             });
-            // $('#container').on('click', '#showActionSheet', function () {
-            //     alert(132);
-            //     var mask = $('#mask');
-            //     var weuiActionsheet = $('#weui_actionsheet');
-            //     weuiActionsheet.addClass('weui_actionsheet_toggle');
-            //     mask.show()
-            //         .focus()//加focus是为了触发一次页面的重排(reflow or layout thrashing),使mask的transition动画得以正常触发
-            //         .addClass('weui_fade_toggle').one('click', function () {
-            //         hideActionSheet(weuiActionsheet, mask);
-            //     });
-            //     $('#actionsheet_cancel').one('click', function () {
-            //         hideActionSheet(weuiActionsheet, mask);
-            //     });
-            //     mask.unbind('transitionend').unbind('webkitTransitionEnd');
-            //
-            //     function hideActionSheet(weuiActionsheet, mask) {
-            //         weuiActionsheet.removeClass('weui_actionsheet_toggle');
-            //         mask.removeClass('weui_fade_toggle');
-            //         mask.on('transitionend', function () {
-            //             mask.hide();
-            //         }).on('webkitTransitionEnd', function () {
-            //             mask.hide();
-            //         })
-            //     }
-            // });
+            $("#go_to").click(function(){
+                var currentItem = $("#container >div").attr("class");
+                var _this = $(this);
+                setUrl("goto",_this,currentItem,2);
+            });
         }
     };
 
-    router.push(first)
-        .setDefault('/first')
+    var item_2 = {
+        url: '/item_2',
+        className: 'item_2',
+        render: function () {
+            return $('#tpl_2').html();
+        },
+        bind: function () {
+            console.info($("#container >div").attr("class"));
+        }
+    };
+
+    //快速定位的js
+    $('#showActionSheet').click(function () {
+        var mask = $('#mask');
+        var weuiActionsheet = $('#weui_actionsheet');
+        weuiActionsheet.addClass('weui_actionsheet_toggle');
+        mask.show()
+            .focus()//加focus是为了触发一次页面的重排(reflow or layout thrashing),使mask的transition动画得以正常触发
+            .addClass('weui_fade_toggle').one('click', function () {
+            hideActionSheet(weuiActionsheet, mask);
+        });
+        $('#actionsheet_cancel').one('click', function () {
+            hideActionSheet(weuiActionsheet, mask);
+        });
+        mask.unbind('transitionend').unbind('webkitTransitionEnd');
+
+        function hideActionSheet(weuiActionsheet, mask) {
+            weuiActionsheet.removeClass('weui_actionsheet_toggle');
+            mask.removeClass('weui_fade_toggle');
+            mask.on('transitionend', function () {
+                mask.hide();
+            }).on('webkitTransitionEnd', function () {
+                mask.hide();
+            })
+        }
+    });
+    router.push(item_1)
+        .push(item_2)
+        .setDefault('/item_1')
         .init();
 });
